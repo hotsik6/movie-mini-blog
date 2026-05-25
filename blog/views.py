8be8efddf91c3901b0e13484a8b0fd0863af8605
hotsik6 <hotsik6@gmail.com>
2026-05-25 20:29:11 +0300

    Created home view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
+
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+
+def home(request):
+
+    posts = Post.objects.all().order_by('-created_at')
+
+    return render(request, 'blog/home.html', {
+        'posts': posts
+    })
